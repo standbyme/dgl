6b30fc9167e1990fb888d14c485b292a9c6227ab
Hongyu Cai <h.tsai@hotmail.com>
2021-01-17 14:31:49 +0800

    PreDataLoaderIter

--- python/dgl/dataloading/pytorch/prefetch.py
+++ python/dgl/dataloading/pytorch/prefetch.py
@@ -2,6 +2,33 @@ from concurrent.futures.thread import ThreadPoolExecutor
 from queue import Queue
 
 
+class PreDataLoaderIter:
+    def __init__(self, dataloader_iter):
+        self.dataloader_iter = dataloader_iter
+        self.prefetch_next_executor = ThreadPoolExecutor(max_workers=1)
+        self.queue = Queue()
+        self.prefetch_next_executor.submit(self.prefetch__next__)
+
+    def __next__(self):
+        v = self.queue.get()
+
+        if v is None:
+            raise StopIteration
+        return v
+
+    def prefetch__next__(self):
+        while True:
+            try:
+                result = self.raw__next__()
+                self.queue.put(result)
+            except StopIteration:
+                self.queue.put(None)
+                break
+
+    def raw__next__(self):
+        return self.dataloader_iter.__next__()
+
+
 class PreDataLoader:
     def __init__(self, dataloader, num_epochs: int):
         self.dataloader = dataloader
@@ -22,7 +49,7 @@ class PreDataLoader:
 
     def pre__iter__(self):
         for _ in range(self.num_epochs):
-            self.prefetch_iter_executor.submit(lambda: self.queue.put(self.raw__iter__()))
+            self.prefetch_iter_executor.submit(lambda: self.queue.put(PreDataLoaderIter(self.raw__iter__())))
 
     def raw__iter__(self):
         return self.dataloader.__iter__()
